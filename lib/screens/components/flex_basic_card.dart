d9b213ce05e5d3461da21c9eb5712265057a5453
Malcolm <mmachiri@dbsglobal.co.zw>
2022-03-25 09:32:15 +0200

    --code cleanup

--- lib/screens/components/flex_basic_card.dart
+++ lib/screens/components/flex_basic_card.dart
@@ -23,9 +23,9 @@ class FlexBasicCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Slidable(
-      showAllActionsThreshold: 0.5,
-      actionPane: SlidableScrollActionPane(),
-      actionExtentRatio: 1 / 9,
+      // showAllActionsThreshold: 0.5,
+      // actionPane: SlidableScrollActionPane(),
+      // actionExtentRatio: 1 / 9,
       child: Card(
         margin: EdgeInsets.only(top: 24, bottom: 8),
         child:
@@ -53,41 +53,41 @@ class FlexBasicCard extends StatelessWidget {
           ),
         ]),
       ),
-      secondaryActions: <Widget>[
-        SlideAction(
-          onTap: this.morePress,
-          child: FlexIconButton(
-            borderColor: Color(0XFFA2A2A2), // Could make this concrete since
-            //its the same for all icons and is being repeated below
-            iconColor: Color(0XFF00A500),
-            icon: Icons.info,
-          ),
-        ),
-        SlideAction(
-          onTap: this.editPress,
-          child: FlexIconButton(
-            borderColor: Color(0XFFA2A2A2), //DRY DRY DRY
-            iconColor: Color(0XFFA2A2A2), //DRY DRY DRY
-            icon: Icons.edit,
-          ),
-        ),
-        SlideAction(
-          onTap: this.morePress,
-          child: FlexIconButton(
-            borderColor: Color(0XFFA2A2A2),
-            iconColor: Color(0XFFA2A2A2),
-            icon: Icons.near_me,
-          ),
-        ),
-        SlideAction(
-          onTap: this.deletePress,
-          child: FlexIconButton(
-            borderColor: Color(0XFFA2A2A2),
-            iconColor: Color(0XFFA2A2A2),
-            icon: Icons.delete,
-          ),
-        ),
-      ],
+      // secondaryActions: <Widget>[
+      //   SlideAction(
+      //     onTap: this.morePress,
+      //     child: FlexIconButton(
+      //       borderColor: Color(0XFFA2A2A2), // Could make this concrete since
+      //       //its the same for all icons and is being repeated below
+      //       iconColor: Color(0XFF00A500),
+      //       icon: Icons.info,
+      //     ),
+      //   ),
+      //   SlideAction(
+      //     onTap: this.editPress,
+      //     child: FlexIconButton(
+      //       borderColor: Color(0XFFA2A2A2), //DRY DRY DRY
+      //       iconColor: Color(0XFFA2A2A2), //DRY DRY DRY
+      //       icon: Icons.edit,
+      //     ),
+      //   ),
+      //   SlideAction(
+      //     onTap: this.morePress,
+      //     child: FlexIconButton(
+      //       borderColor: Color(0XFFA2A2A2),
+      //       iconColor: Color(0XFFA2A2A2),
+      //       icon: Icons.near_me,
+      //     ),
+      //   ),
+      //   SlideAction(
+      //     onTap: this.deletePress,
+      //     child: FlexIconButton(
+      //       borderColor: Color(0XFFA2A2A2),
+      //       iconColor: Color(0XFFA2A2A2),
+      //       icon: Icons.delete,
+      //     ),
+      //   ),
+      // ],
     );
   }
 }
